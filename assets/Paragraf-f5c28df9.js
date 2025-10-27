33853ec62cd41af9e3f1f4b89a010d7e8dbd3582
Farriq-mfq <farriqmuwaffaq100@gmail.com>
2025-10-27 13:56:08 +0700

    deploy

---
+++ assets/Paragraf-f5c28df9.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as r,l as a}from"./index-fccce9c2.js";const c={},t={class:"leading-7"};function n(e,_){return o(),r("p",t,[a(e.$slots,"default")])}const f=s(c,[["render",n]]);export{f as P};
